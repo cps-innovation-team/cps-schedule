74287d96d8168f9e1532a42b84fab44194938a91
Rahim Malik <amasraz.malik@gmail.com>
2024-08-26 21:33:03 -0400

    Delete Build directory

--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Schedule (iOS).build/DerivedSources/Pods_Schedule__iOS__vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_Schedule__iOS_VersionString[];
- extern const double Pods_Schedule__iOS_VersionNumber;
-
- const unsigned char Pods_Schedule__iOS_VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Schedule__iOS_  PROJECT:Pods-1" "\n";
- const double Pods_Schedule__iOS_VersionNumber __attribute__ ((used)) = (double)1.;
